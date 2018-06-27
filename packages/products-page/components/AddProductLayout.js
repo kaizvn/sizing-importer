4bae439532cc52ffd5dd9782367833a7854c24c6
Kai <kai@gobike.asia>
2018-06-28 00:40:20 +0700

    implement the transform rawData

--- packages/products-page/components/AddProductLayout.js
+++ packages/products-page/components/AddProductLayout.js
@@ -3,13 +3,6 @@ import React from 'react';
 import AddProductPage from './AddProductPage';
 import Header from './Header'
 
-const state = {
-    currentPage: 'ProductPage',
-    title: 'Air Canada 2017',
-    dateFrom: 'Feb 2017',
-    dateTo: 'Jul 2017'
-}
-
 export default (props) => (
   <div style={{  width: '70%', margin: '0 auto' }}>
     <Header title={`${props.title} | ${props.project}`} />
--- packages/products-page/components/AddProductPage.js
+++ packages/products-page/components/AddProductPage.js
@@ -2,6 +2,8 @@ import DropToUpload from 'react-drop-to-upload';
 import React from 'react';
 import XLSX from 'xlsx';
 
+let importData;
+
 const buttonStyle = {
   backgroundColor: '#6458f5',
   color: '#fff',
@@ -21,37 +23,101 @@ const wrapperStyle = {
 
 /* generate an array of column objects */
 const make_cols = refstr => {
-	let o = [], C = XLSX.utils.decode_range(refstr).e.c + 1;
-	for(var i = 0; i < C; ++i) o[i] = {name:XLSX.utils.encode_col(i), key:i}
-	return o;
+  let o = [],
+    C = XLSX.utils.decode_range(refstr).e.c + 1;
+  for (var i = 0; i < C; ++i) o[i] = { name: XLSX.utils.encode_col(i), key: i };
+  return o;
 };
 
-const handleFile = (file) => {
-		/* Boilerplate to set up FileReader */
-		const reader = new FileReader();
-		const rABS = !!reader.readAsBinaryString;
-		reader.onload = (e) => {
-			/* Parse data */
-			const bstr = e.target.result;
-			const wb = XLSX.read(bstr, {type:rABS ? 'binary' : 'array'});
-			/* Get first worksheet */
-			const wsname = wb.SheetNames[0];
-			const ws = wb.Sheets[wsname];
-			/* Convert array of arrays */
-			const data = XLSX.utils.sheet_to_json(ws, {header:1});
-			/* Update state */
-			console.log({ data: data, cols: make_cols(ws['!ref']) });
-		};
-		if(rABS) reader.readAsBinaryString(file); else reader.readAsArrayBuffer(file);
-	};
+const handleFile = file => {
+  /* Boilerplate to set up FileReader */
+  const reader = new FileReader();
+  const rABS = !!reader.readAsBinaryString;
+  const fileName = file.name;
+  reader.onload = e => {
+    /* Parse data */
+    const bstr = e.target.result;
+    const wb = XLSX.read(bstr, { type: rABS ? 'binary' : 'array' });
+    /* Get first worksheet */
+    const wsname = wb.SheetNames[0];
+    const ws = wb.Sheets[wsname];
+    /* Convert array of arrays */
+    const rawData = XLSX.utils.sheet_to_json(ws, { header: 1 });
+    importData = transformRawToData(fileName.trim(), rawData);
+
+    console.log('importData:', importData);
+    //console.log({ data: data, cols: make_cols(ws['!ref']) });
+  };
+
+  rABS ? reader.readAsBinaryString(file) : reader.readAsArrayBuffer(file);
+};
+
+const transformRawToData = (fileName, rawData) => {
+  // proposed format :
+  // output = {  table : {data: [], label: []}, dimensions :  [{name: '', size_name: '', measure: '' , body_measuare: '' }], code: '', base_size: '', category: '', gender: '' }
+  //remember to replace size with waist
+  const baseDemensions = ['size', 'inseam', 'build'];
+  const [x, code, gender, category] = fileName.match(
+    /\[(.*)\] (.* WOMEN|MEN|UNISEX) (.*)\.xlsx$/i
+  ) || ['', '', '', ''];
+
+  const transformedData = {
+    code,
+    category,
+    gender,
+    dimensions: [],
+    base_size: '',
+  };
+
+  let currentDemension;
+  rawData.forEach(row => {
+    if (row && row.length) {
+      const firstItem = (row[0] + '').trim().toLowerCase();
+      if (firstItem === 'base size') {
+        transformedData.base_size = row[1].trim();
+      }
+      // check if row is demension row, and collect label
+      else if (baseDemensions.includes(firstItem)) {
+        // found currentDemension: push completed previous demension into array
+        if (currentDemension) {
+          transformedData.dimensions.push(currentDemension);
+          currentDemension = null;
+        }
+
+        const name = firstItem;
+        const sizes = row.slice(2);
+
+        currentDemension = {
+          name,
+          sizes,
+          label: row,
+          data: [],
+          measures: [],
+        };
+        // data row
+      } else if (currentDemension) {
+        const measures = {
+          garment_measure: row[0] && row[0].trim(),
+          description: row[1] && row[1].trim(),
+          body_measuare: '', //  did not select yet
+        };
+
+        currentDemension.measures.push(measures);
+        currentDemension.data.push(row);
+      }
+    }
+  });
+
+  return transformedData;
+};
 
 const handleDrop = files => {
-  console.log('drop!', files);
-  const file = files[0];
-  handleFile(file);
-  // for(let i = 0; i < files.length; i++) {
-  //
-  // }
+  try {
+    const file = files[0];
+    handleFile(file);
+  } catch (e) {
+    throw e;
+  }
 };
 
 export default props => (
